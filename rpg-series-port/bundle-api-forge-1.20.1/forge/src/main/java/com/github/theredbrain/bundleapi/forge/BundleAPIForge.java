f2e0d4c080fd9da8780664f5049d99563252985f
Herbertofury <stealth.sanchez@hotmail.com>
2026-08-27 13:06:43 -0600

    test: execute Bundle API self-test in Forge runtime

--- rpg-series-port/bundle-api-forge-1.20.1/forge/src/main/java/com/github/theredbrain/bundleapi/forge/BundleAPIForge.java
+++ rpg-series-port/bundle-api-forge-1.20.1/forge/src/main/java/com/github/theredbrain/bundleapi/forge/BundleAPIForge.java
@@ -1,11 +1,22 @@
 package com.github.theredbrain.bundleapi.forge;
 
 import com.github.theredbrain.bundleapi.BundleAPI;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BundleAPI.MOD_ID)
 public final class BundleAPIForge {
     public BundleAPIForge() {
         BundleAPI.init();
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        if ("1".equals(System.getenv("BUNDLE_API_SELF_TEST"))) {
+            event.enqueueWork(BundleAPISelfTest::run);
+        }
     }
 }
