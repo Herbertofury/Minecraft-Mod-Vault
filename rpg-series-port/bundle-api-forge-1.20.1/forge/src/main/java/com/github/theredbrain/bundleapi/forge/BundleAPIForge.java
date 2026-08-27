23938fc851d6b73cd7671b8b56114fe63178db9d
Herbertofury <stealth.sanchez@hotmail.com>
2026-08-27 14:16:35 -0600

    test: attach Bundle API self-test registry before freeze

--- rpg-series-port/bundle-api-forge-1.20.1/forge/src/main/java/com/github/theredbrain/bundleapi/forge/BundleAPIForge.java
+++ rpg-series-port/bundle-api-forge-1.20.1/forge/src/main/java/com/github/theredbrain/bundleapi/forge/BundleAPIForge.java
@@ -8,14 +8,19 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BundleAPI.MOD_ID)
 public final class BundleAPIForge {
+    private static final boolean SELF_TEST_ENABLED = "1".equals(System.getenv("BUNDLE_API_SELF_TEST"));
+
     public BundleAPIForge() {
         BundleAPI.init();
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        if (SELF_TEST_ENABLED) {
+            BundleAPISelfTest.TEST_ITEMS.register(modBus);
+        }
         modBus.addListener(this::commonSetup);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
-        if ("1".equals(System.getenv("BUNDLE_API_SELF_TEST"))) {
+        if (SELF_TEST_ENABLED) {
             event.enqueueWork(BundleAPISelfTest::run);
         }
     }
